1d4187b72bd3b4a30fb9e1db07d6a01de3132653
svc-student <svc-student@student.directory.jccc>
2018-11-06 17:42:50 -0600

    work from 11-6

---
+++ Assets/Scripts/LoadSaveLevelData.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Text;
+
+public class LoadSaveLevelData : MonoBehaviour
+{
+    private LevelData levelData = new LevelData();
+
+    public GameObject obstacleTemplate;
+    public GameObject pickupTemplate;
+    public GameObject objectTemplate;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+[Serializable]
+public class Ground
+{
+    public float width, height, xScale, yScale;
+
+    public Ground()
+    {
+        width = 1;
+        height = 1;
+        xScale = 1;
+        yScale = 1;
+    }
+}
+
+//contains the data from the pickups we want to save
+[Serializable]
+public class PickupData
+{
+    public Color glowColor;
+    public Vector3 position;
+    public Quaternion rotation;
+}
+
+[Serializable]
+public class NonPickupData
+{
+    public Vector3 position;
+    public Quaternion rotation;
+}
+
+[Serializable]
+public class LevelData
+{
+    public string levelName;
+    public Ground groundPlane = new Ground();
+    public List<PickupData> pickupList = new List<PickupData>();
+    public List<NonPickupData> objectList = new List<NonPickupData>();
+
+    #region get/set methods for the level data itself
+    public string GetLevelName()
+    {
+        return levelName;
+    }
+    public void SetLevelName(string levelName)
+    {
+        this.levelName = levelName;
+    }
+    #endregion
+
+    #region save and load levelData functions
+    public static LevelData LoadFromFile(string fileName)
+    {
+        return JsonUtility.FromJson<LevelData>(System.IO.File.ReadAllText(fileName));
+    }//end loadfromfile
+
+    public void SaveToFile(string fileName)
+    {
+        System.IO.File.WriteAllText(fileName, JsonUtility.ToJson(this, true));
+    }//end savetofile
+    #endregion
+}
---
+++ Assets/Scripts/UIControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIControl : MonoBehaviour
+{
+    public Text txtLevelName;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+       
+	}
+
+    public void btnCreateNewLevel()
+    {
+        //switch to blank level
+        //UnityEngine.SceneManagement.SceneManager.UnloadScene("MainMenu");
+        UnityEngine.SceneManagement.SceneManager.LoadScene("EditingScene");
+    }
+
+    public void btnLoadLevel()
+    {
+        //load the data based on its coresponding input box value
+        
+        LevelData.LoadFromFile("Test1");
+    }
+}
